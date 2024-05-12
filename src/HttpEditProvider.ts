7faa8678e8098ada3d61313f1b4c9651b144901f
Ramaraj T <jsramraj@gmail.com>
2024-05-12 19:42:58 +0530

    code freeze

---
+++ src/HttpEditProvider.ts
@@ -0,0 +1,67 @@
+import * as vscode from "vscode";
+import * as path from "path";
+
+export class HttpEditProvider implements vscode.CustomTextEditorProvider {
+  public static register(context: vscode.ExtensionContext): vscode.Disposable {
+    const provider = new HttpEditProvider(context);
+    const providerRegistration = vscode.window.registerCustomEditorProvider(
+      HttpEditProvider.viewType,
+      provider
+    );
+    console.log("HttpEditProvider registered");
+    return providerRegistration;
+  }
+  private static readonly viewType = "restClient.http";
+
+  constructor(private readonly context: vscode.ExtensionContext) {}
+
+  resolveCustomTextEditor(
+    document: vscode.TextDocument,
+    webviewPanel: vscode.WebviewPanel,
+    token: vscode.CancellationToken
+  ): void | Thenable<void> {
+    webviewPanel.webview.options = {
+      enableScripts: true,
+    };
+    webviewPanel.webview.html = this.getWebviewContent();
+  }
+
+  private getWebviewContent(): string {
+    // Local path to main script run in the webview
+    const reactAppPathOnDisk = vscode.Uri.file(
+      path.join(
+        this.context.extensionUri.path,
+        "configViewer",
+        "configViewer.js"
+      )
+    );
+    const reactAppUri = reactAppPathOnDisk.with({ scheme: "vscode-resource" });
+
+    const configJson = `{"description":"This is a file containing a dummy config in order to test a webview react in VS Code.","name":"my config","users":[{"active":true,"name":"alice","roles":["user","admin"]},{"active":true,"name":"bob","roles":["user"]},{"active":false,"name":"charlie","roles":["user"]}]}`;
+
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Config View</title>
+
+        <meta http-equiv="Content-Security-Policy"
+                    content="default-src 'none';
+                             img-src https:;
+                             script-src 'unsafe-eval' 'unsafe-inline' vscode-resource:;
+                             style-src vscode-resource: 'unsafe-inline';">
+
+        <script>
+          window.acquireVsCodeApi = acquireVsCodeApi;
+          window.initialData = ${configJson};
+        </script>
+    </head>
+    <body>
+        <div id="root"></div>
+
+        <script src="${reactAppUri}"></script>
+    </body>
+    </html>`;
+  }
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,31 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import { HttpEditProvider } from "./httpEditorProvider";
+import ViewLoader from "./view/ViewLoader";
+import { HttpEditProvider } from "./HttpEditProvider";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "restclient" is now active!');
+  console.log('Congratulations, your extension "vscode-react" is now active!');
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    "restclient.helloWorld",
+    "extension.viewconfig",
     () => {
-      // The code you place here willx be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from RestClient!");
+      let openDialogOptions: vscode.OpenDialogOptions = {
+        canSelectFiles: true,
+        canSelectFolders: false,
+        canSelectMany: false,
+        filters: {
+          Json: ["json"],
+        },
+      };
+
+      vscode.window
+        .showOpenDialog(openDialogOptions)
+        .then(async (uri: vscode.Uri[] | undefined) => {
+          if (uri && uri.length > 0) {
+            const view = new ViewLoader(uri[0], context.extensionPath);
+          } else {
+            vscode.window.showErrorMessage("No valid file selected!");
+            return;
+          }
+        });
     }
   );
 
   context.subscriptions.push(HttpEditProvider.register(context));
-
   context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+// this method is called when your extension is deactivated
 export function deactivate() {}
